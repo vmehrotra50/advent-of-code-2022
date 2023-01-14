2bb82587a20b573e030ca7dd9d1fe6dd62bfb252
vmehrotra50 <varun.mehrotra50@gmail.com>
2023-01-13 19:42:49 -0500

    starter solution for day 7 pt. 1

--- day-7/device-part-1.cpp
+++ day-7/device-part-1.cpp
@@ -2,4 +2,32 @@
 #include <fstream>
 #include <sstream>
 
-int main() {}
+#include <vector>
+#include <map>
+
+using namespace std;
+
+void parse_line(string& s, vector<string>& out) {
+    stringstream ss(s);
+
+    string s;
+    while (getline(ss, s, ' ')) {
+        out.push_back(s);
+    }
+}
+
+int main() {
+    ifstream file("input.txt");
+    if (!file) return 0;
+
+    map<string, int> file_system;
+
+    string line;
+    string current_dir;
+
+    while (getline(file, line)) {
+        vector<string> parsed;
+        parse_line(line, parsed);
+
+    }
+}
