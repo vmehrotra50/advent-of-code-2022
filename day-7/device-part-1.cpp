7d03f2b24e3fabfdd07057463f43a2b2365ad88e
vmehrotra50 <varun.mehrotra50@gmail.com>
2023-01-13 20:01:53 -0500

    Implement cd

--- day-7/device-part-1.cpp
+++ day-7/device-part-1.cpp
@@ -20,14 +20,32 @@ int main() {
     ifstream file("input.txt");
     if (!file) return 0;
 
-    map<string, int> file_system;
+    map<string, vector<string>> file_system;
+    map<string, int> sizes;
 
     string line;
-    string current_dir;
 
-    while (getline(file, line)) {
+    getline(file, line); // skip over cd to /
+    string current_dir = "/";
+
+    while (true) {
         vector<string> parsed;
         parse_line(line, parsed);
+
+        if (parsed[0] == "$") {
+            if (parsed[1] == "cd") {
+                if (parsed[2] == "..") {
+                    int i = current_dir.length() - 1;
+                    while (current_dir[i] != '/') --i;
+                    current_dir = current_dir.substr(i + 1, current_dir.length());
+                } else {
+                    file_system[current_dir + parsed[2] + "/"] = vector<string>();
+                    sizes[current_dir + parsed[2] + "/"] = 0;
+                }
+            } else if (parsed[2] == "ls") {
+
+            } 
+        }
 
     }
 }
